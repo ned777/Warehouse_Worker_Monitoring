cb6d22195ef65413acf68b444d791379999e7f15
ned777 <nmdcnn@gmail.com>
2025-12-13 04:21:11 -0800

    Updated scanner.py. Asking user to input worker_id and item_id

--- app/scanner.py
+++ app/scanner.py
@@ -3,11 +3,12 @@ from database import get_connection
 conn = get_connection()
 cursor = conn.cursor()
 
+worker_id = int(input("Enter the worker ID: "))
+item_id = int(input("Enter item ID: "))
 
 
 
-
-cursor.execute("SELECT * FROM Worker")
+cursor.execute("SELECT Weight, AisleArea FROM Item Where id = {item_id}")
 print(cursor.fetchall())
 conn.commit()
 conn.close()
