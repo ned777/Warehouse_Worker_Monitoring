eb869bbe09e7bc6c0a17b59440ffa3a8d79ef2bd
ned777 <nmdcnn@gmail.com>
2025-12-14 13:45:26 -0800

    Setup scanner.py to connect to SQLite, accept worker/item input, query Item table, and return weight/aisle

--- app/scanner.py
+++ app/scanner.py
@@ -6,9 +6,19 @@ cursor = conn.cursor()
 worker_id = int(input("Enter the worker ID: "))
 item_id = int(input("Enter item ID: "))
 
+cursor.execute("SELECT Weight, AisleArea FROM Item WHERE id = ?", (item_id,))
+
+result = cursor.fetchone()
+
+if result is None:
+    print("Item not found.")
+    exit()
+
+item_weight, item_aisle = result
+
+print("Item Weight: ", item_weight)
+print("Item Aisle:", item_aisle)
 
 
-cursor.execute("SELECT Weight, AisleArea FROM Item Where id = {item_id}")
-print(cursor.fetchall())
 conn.commit()
 conn.close()
