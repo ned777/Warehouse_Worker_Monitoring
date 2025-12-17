c3f19b5fde37b25efd181db3c5c0a5d2a58f1f31
ned777 <nmdcnn@gmail.com>
2025-12-17 04:09:26 -0800

    Adding user inputs to ScanEvent on scanner.py

--- app/scanner.py
+++ app/scanner.py
@@ -1,4 +1,5 @@
 from database import get_connection
+from datetime import datetime
 
 conn = get_connection()
 cursor = conn.cursor()
@@ -6,7 +7,19 @@ cursor = conn.cursor()
 worker_id = int(input("Enter the worker ID: "))
 item_id = int(input("Enter item ID: "))
 
+mydb = "INSERT INTO ScanEvent (id, worker_id, item_id, timestamp, weight)"
+
+id_ScanEvent = int(input("Enter the value for id: "))
+worker_id_ScanEvent = int(input("Enter worker id: "))
+item_id_ScanEvent = int(input: ("Enter item id: "))
+weight_ScanEvent = int(input("Enter weight: "))
+
+values_ScanEvent = (id_ScanEvent, worker_id_ScanEvent, item_id_ScanEvent, weight_ScanEvent)
+
 cursor.execute("SELECT Weight, AisleArea FROM Item WHERE id = ?", (item_id,))
+
+
+val_Into_ScanEvent = id
 
 result = cursor.fetchone()
 
