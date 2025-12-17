47db33475367ea7090aadc46f92bd4790b6a8c1b
ned777 <nmdcnn@gmail.com>
2025-12-17 06:42:23 -0800

    Sucessfully recorded scanned event

--- app/scanner.py
+++ app/scanner.py
@@ -7,24 +7,7 @@ cursor = conn.cursor()
 worker_id = int(input("Enter the worker ID: "))
 item_id = int(input("Enter item ID: "))
 
-mydb = "INSERT INTO ScanEvent (id, worker_id, item_id, timestamp, weight)"
-
-id_ScanEvent = int(input("Enter the value for id: "))
-worker_id_ScanEvent = int(input("Enter worker id: "))
-item_id_ScanEvent = int(input("Enter item id: "))
-timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-weight_ScanEvent = int(input("Enter weight: "))
-
-values_ScanEvent = (id_ScanEvent, worker_id_ScanEvent, item_id_ScanEvent, timestamp, weight_ScanEvent)
-
-cursor.execute(mydb, values_ScanEvent)
-print(mydb)
-
-cursor.execute("SELECT Weight, AisleArea FROM Item WHERE id = ?", (item_id,))
-
-
-
-
+cursor.execute("SELECT Weight, AisleArea FROM Item WHERE ID = ?", (item_id,))
 result = cursor.fetchone()
 
 if result is None:
@@ -33,8 +16,18 @@ if result is None:
 
 item_weight, item_aisle = result
 
+
 print("Item Weight: ", item_weight)
 print("Item Aisle:", item_aisle)
 
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+cursor.execute(
+    "INSERT INTO ScanEvent (worker_id, item_id, timestamp, weight) VALUES (?,?,?,?)",
+    (worker_id, item_id, timestamp, item_weight)
+)
+
+conn.commit()
 conn.close()
+
+print("Scan event recorded successfully.")
