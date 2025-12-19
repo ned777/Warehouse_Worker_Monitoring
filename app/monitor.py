146fba5bb67f351cf8b60eda0858181ffb38a867
ned777 <nmdcnn@gmail.com>
2025-12-19 06:10:18 -0800

    added weight scale

--- app/monitor.py
+++ app/monitor.py
@@ -1,3 +1,27 @@
 from database import get_connection
 from datetime import datetime, timedelta
 
+conn = get_connection()
+cursor = conn.cursor()
+
+#2 means heavy
+#1 means normal
+#0 means less weight than expected
+
+time_window = datetime.now() - timedelta(minutes=15)
+
+cursor.execute(
+    "SELECT Weight FROM ScanEvent WHERE worker_id = ? AND timestamp >=?",
+    (worker_id, time_window)
+
+scale=[]
+
+if 100<= weight:
+    scale.append(2)
+elif 50<= weight and weight<100:
+    scale.append(1)
+else:
+    scale.append(0)
+
+
+)
