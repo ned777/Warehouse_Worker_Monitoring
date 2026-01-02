75e9215ff32c5c06835bfd173ad822b59773923d
ned777 <nmdcnn@gmail.com>
2026-01-01 22:09:47 -0800

    Add Flask app with /warehouse route and HTML template

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import monitor
 
 app = Flask(__name__)
 
 @app.route("/warehouse")
 def index():
-    return render_templete("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #Start server only when run directly
